c9750b6ac0212f11b6fca78be87cea98c9b014cf
Abhinav N T <abhinavnt639@gmail.com>
2025-06-09 08:59:17 +0530

    object sum recuresion

---
+++ essenstials/sum.js
@@ -0,0 +1,23 @@
+
+function sumrec(obj){
+    let sum=0
+    if(Array.isArray(obj)){
+        for(item of obj){
+            sum+=sumrec(item)
+        }
+    }else if(typeof obj=='object' && obj!==null){
+        for(item in obj){
+            sum+=sumrec(obj[item])
+        }
+    }else if(typeof obj=='number'){
+        sum+=obj
+
+    }
+
+    return sum
+}
+
+
+let obj={a:1,b:[2,{c:3}],d:4,e:{f:5,g:{h:6,i:{j:7,k:8,l:{m:9,n:{o:10}}}}}}
+
+console.log(sumrec(obj));
