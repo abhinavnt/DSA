edf93eff75d5bdca94b79f4c854038d407588aee
Abhinav N T <abhinavnt639@gmail.com>
2025-06-14 10:23:03 +0530

    some code formated

--- problems/timeDifference.js
+++ problems/timeDifference.js
@@ -1,18 +1,14 @@
+function getTimeDifferenceInMinutes(start, end) {
+  const [startHour, startMinute] = start.split(":").map(Number);
+  const [endHour, endMinute] = end.split(":").map(Number);
 
-function getTimeDifferenceInMinutes(start,end){
-    const [startHour,startMinute]=start.split(":").map(Number)
-    const [endHour,endMinute]=end.split(":").map(Number)
-     console.log(startHour,startMinute,"startHour,startMinute");
-     console.log(endHour,endMinute,"endHour,endMinute");
-     
-     
-    const startTotalMinutes=startHour*60+ startMinute;
-    const endTotalMinutes=endHour*60+endMinute
+  const startTotalMinutes = startHour * 60 + startMinute;
+  const endTotalMinutes = endHour * 60 + endMinute;
 
-    return endTotalMinutes-startTotalMinutes
+  return endTotalMinutes - startTotalMinutes;
 }
 
-    const time1 = "10:30";
+const time1 = "10:30";
 const time2 = "16:30";
 
-console.log(getTimeDifferenceInMinutes(time1,time2));
+console.log(getTimeDifferenceInMinutes(time1, time2));
