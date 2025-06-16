bb0c1649f093150de17cdf9cab5c07be4dc00e63
Abhinav N T <abhinavnt639@gmail.com>
2025-06-16 14:55:30 +0530

    flat array using recursion

--- problems/flattenArray.js
+++ problems/flattenArray.js
@@ -1,18 +1,16 @@
 
 let arr=[[1,2,[3,4,[5,6,7,[8,9]],10,11,[12,13]]]]
 
-function flatArray(arr,index=0,result=[]){
-    if(index>=arr.length) return result
-
-    const item=arr[index]
-    if(Array.isArray(item)){
-        flatArray(item,0,result)
-    }else{
-        result.push(item)
+function flatArray(arr){
+    let result=[]
+    for(item of arr){
+        if(Array.isArray(item)){
+            result=result.concat(flatArray(item))
+        }else{
+            result.push(item)
+        }
     }
-
-    return flatArray(arr,index+1,result)
+    return result
 }
-
 
 console.log(flatArray(arr));
