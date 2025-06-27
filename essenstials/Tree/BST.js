4c8dd6e1cb8e5d0d5234715493e34bc8efe2b513
Abhinav N T <abhinavnt639@gmail.com>
2025-06-27 13:30:15 +0530

    second largest element in bst

--- essenstials/Tree/BST.js
+++ essenstials/Tree/BST.js
@@ -5,13 +5,13 @@ class TreeNode {
     this.right = null;
   }
 }
-  
+
 class BST {
   constructor() {
     this.root = null;
   }
 
-//   //check is empty 
+  //   //check is empty
   isEmpty() {
     return this.root == null;
   }
@@ -24,7 +24,6 @@ class BST {
       this.insertNode(this.root, newNode);
     }
   }
-
 
   // //insert value
   insertNode(root, newNode) {
@@ -43,180 +42,187 @@ class BST {
     }
   }
 
+  // //search
 
-// //search 
-
-search(root,value){
-    if(!root){
-        return false
-    }else{
-        if(root.value===value){
-            return true
-        }else if(value<root.value){
-            return this.search(root.left,value)
-        }else{
-            return this.search(root.right,value)
-        }
-    }
-}
-
-
-//  //travesal 
-// //DFS
-//  //preorder
- preOrder(root){
-  if(root){
-    console.log(root.value)
-    this.preOrder(root.left)
-    this.preOrder(root.right)
-    
-  }
- }
-
-
-
-//  //inorder
- inOrder(root){
-  if(root){
-    this.inOrder(root.left)
-    console.log(root.value)
-    this.inOrder(root.right)
-    
-  }
- }
-
-
-//  //post order
- postOrder(root){
-  if(root){
-    this.postOrder(root.left)
-    this.postOrder(root.right)
-    console.log(root.value);
-    
-  }
- }
-
-
-// //  BFS
-// // Level order
-levelOrder(){
-  const queue=[]
-  queue.push(this.root)
-  while(queue.length){
-    let current=queue.shift()
-    console.log(current.value)
-    if(current.left){
-      queue.push(current.left)
-    }
-    if(current.right){
-      queue.push(current.right)
-    }
-    
-  }
-}
-
-
-// //min
-min(root){
-  if(!root.left){
-    return root.value
-  }else{
-    return this.min(root.left)
-  }
-}
-
-
-// //max
-max(root){
-  if(!root.right){
-    return root.value
-  }else{
-    return this.max(root.right)
-  }
-}
-
-//delete
-delete(value){
-  this.root= this.deleteNode(this.root,value)
-}
-
-deleteNode(root,value){
-  if(root ===null){
-    return root 
-  }
-  if(value<root.value){
-    root.left=this.deleteNode(root.left,value)
-  }else if(value>root.value){
-    root.right=this.deleteNode(root.right,value)
-  }else{
-    if(!root.left&& !root.right){
-      return null
-    }
-    if(!root.left){
-      return root.right
-    }else if(!root.right){
-      return root.left
-    }
-    root.value=this.min(root.right) 
-    root.right=this.deleteNode(root.right,root.value)
-  }
-  return root
-}
-
-
-//is valid bst
-isValid(root,min= -Infinity,max= Infinity){
-  if(!root){
-    return true
-  }
-  if(root.value<min||root.value>max){
-    return false
-  }
-  return (this.isValid(root.left,min,root.value)&& this.isValid(root.right,root.value,max)) 
-}
-
-
-//find closest
-findClosest(root,target){
-  let Closest=root.value
-  while(root){
-    if(Math.abs(target-root.value)<Math.abs(target-Closest)){
-      Closest=root.value
-    }
-
-    if(target<root.value){
-      root=root.left
-    }else if(target>root.value){
-      root=root.right
-    }else{
-      break
+  search(root, value) {
+    if (!root) {
+      return false;
+    } else {
+      if (root.value === value) {
+        return true;
+      } else if (value < root.value) {
+        return this.search(root.left, value);
+      } else {
+        return this.search(root.right, value);
+      }
     }
   }
-  return Closest
-}
 
-
-//count of left node
-
-leftcount(root){
-  if(!root.left){
-    return 0
-  }else{
-    return 1+ this.leftcount(root.left)
+  //  //travesal
+  // //DFS
+  //  //preorder
+  preOrder(root) {
+    if (root) {
+      console.log(root.value);
+      this.preOrder(root.left);
+      this.preOrder(root.right);
+    }
   }
-}
 
-//height of tree
-height(node=this.root){
-  if(node===null){
-    return -1
+  //  //inorder
+  inOrder(root) {
+    if (root) {
+      this.inOrder(root.left);
+      console.log(root.value);
+      this.inOrder(root.right);
+    }
   }
-  const leftHeight=this.height(node.left)
-  const rightHeight=this.height(node.right)
 
-  return Math.max(leftHeight,rightHeight)+1
-}
- 
+  //  //post order
+  postOrder(root) {
+    if (root) {
+      this.postOrder(root.left);
+      this.postOrder(root.right);
+      console.log(root.value);
+    }
+  }
+
+  // //  BFS
+  // // Level order
+  levelOrder() {
+    const queue = [];
+    queue.push(this.root);
+    while (queue.length) {
+      let current = queue.shift();
+      console.log(current.value);
+      if (current.left) {
+        queue.push(current.left);
+      }
+      if (current.right) {
+        queue.push(current.right);
+      }
+    }
+  }
+
+  // //min
+  min(root) {
+    if (!root.left) {
+      return root.value;
+    } else {
+      return this.min(root.left);
+    }
+  }
+
+  // //max
+  max(root) {
+    if (!root.right) {
+      return root.value;
+    } else {
+      return this.max(root.right);
+    }
+  }
+
+  //delete
+  delete(value) {
+    this.root = this.deleteNode(this.root, value);
+  }
+
+  deleteNode(root, value) {
+    if (root === null) {
+      return root;
+    }
+    if (value < root.value) {
+      root.left = this.deleteNode(root.left, value);
+    } else if (value > root.value) {
+      root.right = this.deleteNode(root.right, value);
+    } else {
+      if (!root.left && !root.right) {
+        return null;
+      }
+      if (!root.left) {
+        return root.right;
+      } else if (!root.right) {
+        return root.left;
+      }
+      root.value = this.min(root.right);
+      root.right = this.deleteNode(root.right, root.value);
+    }
+    return root;
+  }
+
+  //is valid bst
+  isValid(root, min = -Infinity, max = Infinity) {
+    if (!root) {
+      return true;
+    }
+    if (root.value < min || root.value > max) {
+      return false;
+    }
+    return this.isValid(root.left, min, root.value) && this.isValid(root.right, root.value, max);
+  }
+
+  //find closest
+  findClosest(root, target) {
+    let Closest = root.value;
+    while (root) {
+      if (Math.abs(target - root.value) < Math.abs(target - Closest)) {
+        Closest = root.value;
+      }
+
+      if (target < root.value) {
+        root = root.left;
+      } else if (target > root.value) {
+        root = root.right;
+      } else {
+        break;
+      }
+    }
+    return Closest;
+  }
+
+  //count of left node
+
+  leftcount(root) {
+    if (!root.left) {
+      return 0;
+    } else {
+      return 1 + this.leftcount(root.left);
+    }
+  }
+
+  //height of tree
+  height(node = this.root) {
+    if (node === null) {
+      return -1;
+    }
+    const leftHeight = this.height(node.left);
+    const rightHeight = this.height(node.right);
+
+    return Math.max(leftHeight, rightHeight) + 1;
+  }
+
+  //second largest
+  secondLargest() {
+    if (!this.root || !this.root.left || !this.root.right) return;
+
+    let current = this.root;
+    let parrent = null;
+
+    while (current.right) {
+      parrent = current;
+      current = current.right;
+    }
+
+    if (current.left) {
+      let temp = current.left;
+      while (temp.right) {
+        temp = temp.right;
+      }
+      console.log(temp.value);
+    } else {
+      console.log(parrent.value);
+    }
+  }
 }
 
 let bst = new BST();
@@ -224,8 +230,8 @@ let bst = new BST();
 bst.insert(23);
 bst.insert(234);
 bst.insert(20);
-bst.insert(10)
-bst.insert(300)
+bst.insert(10);
+bst.insert(300);
 // console.log(bst.search(bst.root,53));
 // bst.preOrder(bst.root)
 // console.log(bst.search(bst.root,53));
@@ -240,18 +246,3 @@ bst.insert(300)
 // console.log(bst.isValid(bst.root));
 // console.log(bst.findClosest(bst.root,19));
 console.log(bst.min(bst.root));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
